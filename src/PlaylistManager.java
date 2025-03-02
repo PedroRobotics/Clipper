e47e0f3f5f204d4433fa35b20a3200d66c796579
PedroFoda21 <pedrohenriquedasilva2111@gmail.com>
2025-03-02 00:37:51 -0300

    Estruturando a PlaylistManager

--- src/PlaylistManager.java
+++ src/PlaylistManager.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class PlaylistManager {
+    private static List<Music> playList = new ArrayList<>();
+    private static final String FILE_NAME = "Playlist.txt";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
+class Music {
     
 }
